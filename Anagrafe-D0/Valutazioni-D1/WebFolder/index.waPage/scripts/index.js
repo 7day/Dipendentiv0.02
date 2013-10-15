6ec70fc57ed65ae1814073848701d9d5b073cff6
7day <sviluppatoresw@gmail.com>
2013-10-16 00:17:19 +0200

    cambio

--- Anagrafe-D0/Valutazioni-D1/WebFolder/index.waPage/scripts/index.js
+++ Anagrafe-D0/Valutazioni-D1/WebFolder/index.waPage/scripts/index.js
@@ -9,20 +9,7 @@ var tabella='d1anagdipe';
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
-		try {
-				Rpc2.testconnessione();
-	 		}
-			catch(e) { // in case of error
-	    		var scelta=window.confirm("Db non trovato! vuole cambiare i dati di connessione?");
-				if (scelta){
-					//window.location.replace("/configurazione.waPage/index.html");
-					
-					document.location.href = "/configurazione.waPage/index.html";
-				}
-	 		}
-		//console.log(test);
-	// document.getElementById('myFreezeDiv').style.width="100%";
-	//	document.getElementById('myFreezeDiv').style.height="100%";
+		document.location.href = "/Menu.waPage/index.html";
 	};// @lock
 
 // @region eventManager// @startlock
